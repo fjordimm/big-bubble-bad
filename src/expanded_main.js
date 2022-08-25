ef3fcde3050c39031662d21353d4aa18d2766abf
fjordimm <fjordimm@gmail.com>
2022-08-25 16:52:44 -0700

    asodifjafksdo

--- src/expanded_main.js
+++ src/expanded_main.js
@@ -24,13 +24,17 @@ const ANIMALGENRANDOFFSET = 300;
 const ANIMALSIZE = 50;
 const ANIMALMAXVEL = 0.8;
 const ANIMALMAXROTVEL = 0.04;
+const CAPTUREDANIMALRANDSPEED = 1.5;
 
 let g_mainMouse;
 let g_mainKeyboard;
 let g_mainCamera;
 let g_mainBackground;
+let g_layer;
 let g_player;
 let g_playerBubble;
+let g_ship;
+let g_button;
 
 let g_asteroidDict = {};
 let g_animalDict = {};
@@ -48,30 +52,82 @@ async function OnBeforeProjectStart(runtime)
 	g_mainKeyboard = runtime.objects.MainKeyboard;
 	g_mainCamera = runtime.objects.MainCamera;
 	g_mainBackground = runtime.objects.MainBackground.getFirstInstance();
+	g_layer = runtime.layout.getLayer(LAYER);
 	g_player = runtime.objects.Player.getFirstInstance();
 	g_player.g_xVel = 0;
 	g_player.g_yVel = 0;
 	g_playerBubble = runtime.objects.Bubble.createInstance(LAYER, 0, 0);
 	g_playerBubble.g_bubbleSize = 7;
 	g_playerBubble.g_bubbleType = "playerBub";
+	g_ship = runtime.objects.Ship.getFirstInstance();
+	g_button = runtime.objects.Button.getFirstInstance();
 	
 	g_player.x = 0;
 	g_player.y = 0;
 }
 
+let Tick_alreadyAtShip = true;
+let Tick_clickAlreadyDown = false;
 let Tick_counter = 0;
 function Tick(runtime)
 {
 	Tick_counter++;
-
-	g_mainCamera.lookAtPosition(g_player.x, g_player.y, CAMERAZ, g_player.x, g_player.y, 0, 0, 1, 0);
-
-	g_mainBackground.x = g_player.x;
-	g_mainBackground.y = g_player.y;
 	
-	g_playerBubble.x = g_player.x;
-	g_playerBubble.y = g_player.y;
-	
+	if (g_mainMouse.isMouseButtonDown(0))
+	{
+		const mouseIsOverButton = g_mainMouse.getMouseX() > g_button.x - g_button.width / 2 && g_mainMouse.getMouseX() < g_button.x + g_button.width / 2 && g_mainMouse.getMouseY() > g_button.y - g_button.height / 2 && g_mainMouse.getMouseY() < g_button.y + g_button.height / 2;
+
+		if (!Tick_clickAlreadyDown)
+		{
+			Tick_clickAlreadyDown = true;
+
+			if (mouseIsOverButton)
+			{
+				alert("buton");
+				g_layer.isVisible = false;
+			}
+		}
+
+		if (!mouseIsOverButton)
+		{
+			const mouseX = g_mainMouse.getMouseX();
+			const mouseY = g_mainMouse.getMouseY();
+		
+			const xDisp = mouseX - g_player.x;
+			const yDisp = mouseY - g_player.y;
+			const angle = Math.atan(yDisp / xDisp);
+			const thingus = (Math.sign(xDisp) == -1) ? Math.PI : 0;
+			const xDelta = -Math.cos(angle + thingus);
+			const yDelta = -Math.sin(angle + thingus);
+
+			g_player.g_xVel += xDelta * PLAYERACC;
+			g_player.g_yVel += yDelta * PLAYERACC;
+			
+			if (Tick_counter % BUBBLESHOOTINCR == 0)
+			{
+				const shotBubble = runtime.objects.Bubble.createInstance(LAYER, g_player.x, g_player.y);
+				shotBubble.g_bubbleSize = 1;
+				shotBubble.g_bubbleType = "shotBub";
+				shotBubble.g_popTime = BUBBLESHOOTDESTROYDELAY;
+
+				const finalBubbleShootRand = BUBBLESHOOTRANDMULT * Math.exp(-BUBBLESHOOTRANDSCALE * (g_player.g_xVel * xDelta + g_player.g_yVel * yDelta));
+				const angleMB = angle + Math.random() * finalBubbleShootRand - 0.5 * finalBubbleShootRand;
+				const thingusMB = (Math.sign(xDisp) == -1) ? Math.PI : 0;
+				const xDeltaMB = -Math.cos(angleMB + thingusMB);
+				const yDeltaMB = -Math.sin(angleMB + thingusMB);
+
+				shotBubble.g_xVel = -xDeltaMB * BUBBLESHOOTERACC + g_player.g_xVel;
+				shotBubble.g_yVel = -yDeltaMB * BUBBLESHOOTERACC + g_player.g_yVel;
+
+				g_playerBubble.g_bubbleSize -= 0.01;
+			}
+		}
+	}
+	else
+	{
+		Tick_clickAlreadyDown = false;
+	}
+
 	if (g_mainMouse.isMouseButtonDown(0))
 	{
 		const mouseX = g_mainMouse.getMouseX();
@@ -92,7 +148,7 @@ function Tick(runtime)
 			const shotBubble = runtime.objects.Bubble.createInstance(LAYER, g_player.x, g_player.y);
 			shotBubble.g_bubbleSize = 1;
 			shotBubble.g_bubbleType = "shotBub";
-			shotBubble.g_timeSinceShot = 0;
+			shotBubble.g_popTime = BUBBLESHOOTDESTROYDELAY;
 
 			const finalBubbleShootRand = BUBBLESHOOTRANDMULT * Math.exp(-BUBBLESHOOTRANDSCALE * (g_player.g_xVel * xDelta + g_player.g_yVel * yDelta));
 			const angleMB = angle + Math.random() * finalBubbleShootRand - 0.5 * finalBubbleShootRand;
@@ -110,29 +166,73 @@ function Tick(runtime)
 	g_player.x += g_player.g_xVel;
 	g_player.y += g_player.g_yVel;
 
-	const allBubbles = runtime.objects.Bubble.getAllInstances();
-	for (let i = 0; i < allBubbles.length; i++)
 	{
-		allBubbles[i].width = BUBBLESIZEMULT * allBubbles[i].g_bubbleSize;
-		allBubbles[i].height = BUBBLESIZEMULT * allBubbles[i].g_bubbleSize;
-		
-		allBubbles[i].x += allBubbles[i].g_xVel;
-		allBubbles[i].y += allBubbles[i].g_yVel;
-
-		if (allBubbles[i].g_bubbleType === "shotBub")
+		const allBubbles = runtime.objects.Bubble.getAllInstances();
+		for (let i = 0; i < allBubbles.length; i++)
 		{
-			allBubbles[i].g_timeSinceShot++;
-			if (allBubbles[i].g_timeSinceShot > BUBBLESHOOTDESTROYDELAY)
-			{ allBubbles[i].destroy(); }
+			allBubbles[i].width = BUBBLESIZEMULT * allBubbles[i].g_bubbleSize;
+			allBubbles[i].height = BUBBLESIZEMULT * allBubbles[i].g_bubbleSize;
+			
+			allBubbles[i].x += allBubbles[i].g_xVel;
+			allBubbles[i].y += allBubbles[i].g_yVel;
+
+			if (allBubbles[i].g_bubbleType === "shotBub")
+			{
+				allBubbles[i].g_popTime--;
+				if (allBubbles[i].g_popTime <= 0)
+				{ allBubbles[i].destroy(); }
+			}
 		}
 	}
 
 	const allAnimals = runtime.objects.Animal.getAllInstances();
 	for (let i = 0; i < allAnimals.length; i++)
+	{
+		if (allAnimals[i].g_isCaptured)
+		{
+			let dist = Math.sqrt(Math.pow(g_playerBubble.x - allAnimals[i].x, 2) + Math.pow(g_playerBubble.y - allAnimals[i].y, 2));
+			if (dist > 0.1 + Math.min(g_playerBubble.width, g_playerBubble.height) / 2 - Math.min(allAnimals[i].width, allAnimals[i].height) / 2)
+			{
+				allAnimals[i].g_bubRelX -= allAnimals[i].g_bubRelVelX * 2;
+				allAnimals[i].g_bubRelY -= allAnimals[i].g_bubRelVelY * 2;
+			}
+			if (dist > Math.min(g_playerBubble.width, g_playerBubble.height) / 2 - Math.min(allAnimals[i].width, allAnimals[i].height) / 2)
+			{
+				allAnimals[i].g_bubRelVelX *= -1;
+				allAnimals[i].g_bubRelVelY *= -1;
+			}
+			
+			allAnimals[i].g_bubRelX += allAnimals[i].g_bubRelVelX;
+			allAnimals[i].g_bubRelY += allAnimals[i].g_bubRelVelY;
+
+			allAnimals[i].x = g_playerBubble.x + allAnimals[i].g_bubRelX;
+			allAnimals[i].y = g_playerBubble.y + allAnimals[i].g_bubRelY;
+		}
+		else
+		{
+			allAnimals[i].x += allAnimals[i].g_xVel;
+			allAnimals[i].y += allAnimals[i].g_yVel;
+			allAnimals[i].angle += allAnimals[i].g_rotVel;
+
+			if (allAnimals[i].testOverlap(g_playerBubble))
+			{
+				allAnimals[i].g_isCaptured = true;
+				allAnimals[i].g_bubRelX = 0;
+				allAnimals[i].g_bubRelY = 0;
+				allAnimals[i].g_bubRelVelX = Math.random() * CAPTUREDANIMALRANDSPEED - 0.5 * CAPTUREDANIMALRANDSPEED;
+				allAnimals[i].g_bubRelVelY = Math.random() * CAPTUREDANIMALRANDSPEED - 0.5 * CAPTUREDANIMALRANDSPEED;
+			}
+		}
+	}
+
+	g_player.width = 31.586345;
+	const allAsteroids = runtime.objects.Asteroid.getAllInstances();
+	for (let i = 0; i < allAsteroids.length; i++)
 	{		
-		allAnimals[i].x += allAnimals[i].g_xVel;
-		allAnimals[i].y += allAnimals[i].g_yVel;
-		allAnimals[i].angle += allAnimals[i].g_rotVel;
+		if (allAsteroids[i].testOverlap(g_playerBubble))
+		{
+			g_player.width = 10;
+		}
 	}
 
 	for (let c = -ASTEROIDGENRENDDIST; c < ASTEROIDGENRENDDIST; c++)
@@ -169,6 +269,7 @@ function Tick(runtime)
 				const randOffsetX = Math.random() * ANIMALGENRANDOFFSET - 0.5 * ANIMALGENRANDOFFSET;
 				const randOffsetY = Math.random() * ANIMALGENRANDOFFSET - 0.5 * ANIMALGENRANDOFFSET;
 				let newAnimal = runtime.objects.Animal.createInstance(LAYER, animalX * ANIMALGENSPACING + randOffsetX, animalY * ANIMALGENSPACING + randOffsetY);
+				newAnimal.g_isCaptured = false;
 				newAnimal.width = ANIMALSIZE;
 				newAnimal.height = ANIMALSIZE;
 				newAnimal.angle = Math.random() * 2 * Math.PI;
@@ -178,4 +279,48 @@ function Tick(runtime)
 			}
 		}
 	}
+
+	if (g_player.testOverlap(g_ship))
+	{
+		if (!Tick_alreadyAtShip)
+		{
+			Tick_alreadyAtShip = true;
+
+			let capturedAnimalCount = 0;
+			const allAnimals = runtime.objects.Animal.getAllInstances();
+			for (let i = 0; i < allAnimals.length; i++)
+			{
+				if (allAnimals[i].g_isCaptured)
+				{
+					capturedAnimalCount++;
+					allAnimals[i].destroy();
+				}
+			}
+
+			alert(`You recovered ${capturedAnimalCount} animals!`);
+		}
+	}
+	else
+	{
+		Tick_alreadyAtShip = false;
+	}
+
+	g_mainCamera.lookAtPosition(g_player.x, g_player.y, CAMERAZ, g_player.x, g_player.y, 0, 0, 1, 0);
+
+	g_mainBackground.x = g_player.x;
+	g_mainBackground.y = g_player.y;
+	
+	g_playerBubble.x = g_player.x;
+	g_playerBubble.y = g_player.y;
+
+	g_player.moveToTop();
+	g_playerBubble.moveToTop();
 }
+
+/*
+function areOverlapping(a, b)
+{
+	let dist = Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
+	return dist < Math.min(a.width, a.height) / 2 + Math.min(b.width, b.height) / 2;
+}
+*/
